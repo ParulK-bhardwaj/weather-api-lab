70211836c29840eeae438bff8add10cc37c66494
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2023-09-11 10:51:29 -0400

    challenge 3

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 
 // Functions 
-function getWeather(zip, apiKey, onComplete) {
+function getWeather(zip, apiKey) {
 // Replace this with your own API key!
 const units = 'imperial'
 const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-fetch(path)
+return fetch(path)
     .then(res => res.json())
-    .then(json => {
-        onComplete(json)
-    })
-    .catch(err => console.log(err.message))
 }
-  
              
