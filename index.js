66e9ca1472ea338319191335879a3b69f8cbab5a
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2023-09-11 13:53:29 -0400

    challenge 7

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-
 // Functions 
-async function getWeather(zip, apiKey, unit = 'imperial') {
-    // Replace this with your own API key!
-    const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
-    // code stops here and waits for promise to resolve
+
+async function fetchWeather(path) {
+    // waits for promise to resolve
     const res = await fetch(path) 
     // waits here for promise to resolve
     const json = await res.json() 
     // Returns json wrapped in a promise!
     // return data
-    const formattedData = {
+    if(json.cod != 200) {
+        return json
+    }
+    return {
         temp: json.main.temp,
         desc: json.weather[0].description,
         pressure: json.main.pressure,
@@ -22,6 +22,23 @@ async function getWeather(zip, apiKey, unit = 'imperial') {
         city: json.name,
         wind: json.wind.speed,
     }
-    return formattedData
 }
              
+
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+
+async function getWeatherByZip(apiKey, zip, unit = 'imperial') {
+    const path = `${baseUrl}zip=${zip}&appid=${apiKey}&units=${unit}`
+    return await fetchWeather(path)
+}
+
+
+async function getWeatherByCity(apiKey, city, unit = 'imperial') {
+    const path = `${baseUrl}q=${city}&appid=${apiKey}&units=${unit}`;
+    return await fetchWeather(path);
+}
+
+async function getWeatherByGeo(apiKey, lat, lon, unit = 'imperial') {
+    const path = `${baseUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`
+    return await fetchWeather(path)
+}
